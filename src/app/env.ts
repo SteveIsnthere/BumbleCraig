e07b453516769e91501b96bbc92028a02516f46b
steve <dogshitforreal@gmail.com>
2023-07-25 01:34:12 -0700

    added tons of shit

--- src/app/env.ts
+++ src/app/env.ts
@@ -4,7 +4,9 @@ export const colors = [
   "beige", "bisque", "chocolate", "cornsilk", "darkcyan", "darkslategray"
 ];
 
-export const apiEndPoint: string = 'http://127.0.0.1:8000/tidder'
+// export const apiEndPoint: string = 'http://127.0.0.1:8000/tidder'
+export const apiEndPoint: string = 'https://c490-2001-569-58aa-1400-f9bc-a02c-174-ae1b.ngrok-free.app/tidder'
+
 
 export const rankingModes = [
   'Recommended',
