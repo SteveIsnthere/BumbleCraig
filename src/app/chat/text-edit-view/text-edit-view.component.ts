8d8f41b2077e7ea5958468954cb21c9076a2816f
priva <npminitfailed@gmail.com>
2023-11-05 01:34:06 -0700

    added tons of shit

--- src/app/chat/text-edit-view/text-edit-view.component.ts
+++ src/app/chat/text-edit-view/text-edit-view.component.ts
@@ -9,21 +9,29 @@ import {MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef} from "@angular/material/bottom
 })
 export class TextEditViewComponent {
   prompt: string = '';
-  textContent: string | null = null;
+  // textContent: string | null = null;
 
   constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public title: string, private http: HttpClient, private bottomSheetRef: MatBottomSheetRef<TextEditViewComponent>) {
     this.prompt = title;
   }
 
-  changeContent(event: any) {
-    this.textContent = event.target.value;
-  }
+  // changeContent(event: any) {
+  //   this.textContent = event.target.value;
+  // }
 
 
-  upload() {
-    if (this.textContent == null) {
+  // upload() {
+  //   if (this.textContent == null) {
+  //     return
+  //   }
+  //   this.bottomSheetRef.dismiss(this.textContent);
+  // }
+
+  upload(inputElement: any) {
+    let textContent = inputElement.value;
+    if (textContent == null) {
       return
     }
-    this.bottomSheetRef.dismiss(this.textContent);
+    this.bottomSheetRef.dismiss(textContent);
   }
 }
