eb0e2f7aa4feb54fb74d5c41d2f0186c60aa3d6a
priva <npminitfailed@gmail.com>
2023-12-14 21:39:18 -0800

    added tons of shit

--- src/app/env.ts
+++ src/app/env.ts
@@ -6,8 +6,8 @@ export const colors = [
   "#263238", "#90a4ae", "#ec407a", "#ab47bc", "#42a5f5"
 ];
 
-export const apiEndPoint: string = 'http://127.0.0.1:8000/tidder'
-// export const apiEndPoint: string = 'https://dogshit.ngrok.app/tidder'
+// export const apiEndPoint: string = 'http://127.0.0.1:8000/tidder'
+export const apiEndPoint: string = 'https://dogshit.ngrok.app/tidder'
 
 export const rankingModes = [
   'Recommended',
